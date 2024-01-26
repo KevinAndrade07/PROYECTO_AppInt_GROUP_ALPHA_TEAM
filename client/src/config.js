795f32f14ed2088e7227e3b11f306d7e07a2acd1
KevinAndrade07 <kdasandrades@gmail.com>
2024-01-25 23:37:54 -0500

    direccion de api alojada en render

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    nombreserver: 'http://localhost:19905'
+    //nombreserver: 'http://localhost:19905'
+    nombreserver: 'https://api-proyecto-app-int.onrender.com'
   };
   
   module.exports = config;
